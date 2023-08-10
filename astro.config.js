7ed644ec921b37a91eafbc64ef8a93dc29e93664
Th1nkK1D <witheep@gmail.com>
2023-08-10 12:22:01 +0700

    chore: config prettier and commitlint

--- astro.config.js
+++ astro.config.js
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+	integrations: [svelte(), tailwind()],
 });
