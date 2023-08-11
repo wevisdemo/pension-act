8feabc76b9fee7111068b7f4c67ed5589667da00
Th1nkK1D <witheep@gmail.com>
2023-08-11 17:38:39 +0700

    feat: add custom color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,21 @@ module.exports = {
 	},
 	plugins: [require('daisyui')],
 	daisyui: {
-		themes: ['light'],
+		themes: [
+			{
+				codeforall: {
+					primary: '#2E3283',
+					'primary-focus': '#4349BA',
+					secondary: '#2BB25C',
+					'secondary-focus': '#35D26E',
+					accent: '#8054FF',
+					'accent-focus': '#5929E3',
+					neutral: '#000000',
+					'base-100': '#FFFFFF',
+					'base-300': '#E5E6E6',
+					info: '#F8EB54',
+				},
+			},
+		],
 	},
 };
