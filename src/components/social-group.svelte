5f1a9dd892ebe8a15f13a0f026a8e7f964c7ba91
petchill <songpon111333@gmail.com>
2023-08-12 22:25:57 +0700

    feat(#1): add welcome page but still has required some link

---
+++ src/components/social-group.svelte
@@ -0,0 +1,10 @@
+<script lang="ts"></script>
+
+<div class="flex [&>*:not(:first-child)]:ml-[4px] justify-center">
+	<!-- TODO: add social share -->
+	<img src="/facebook-logo.svg" alt="facebook-logo" />
+	<img src="/x-logo.svg" alt="x-logo" />
+	<img src="/line-logo.svg" alt="line-logo" />
+</div>
+
+<style></style>
---
+++ src/components/welcome.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import SocialGroup from './social-group.svelte';
+</script>
+
+<div class="flex flex-col md:flex-row">
+	<div>
+		<img
+			class="w-[180px] md:w-[280px] max-w-[280px] m-auto"
+			src="/campaign-logo.svg"
+			alt="campaign-logo"
+		/>
+		<p
+			class="heading-responsive-01 text-info flex flex-col justify-center items-center"
+		>
+			<span> “เสนอคำถามประชามติ </span>
+			<span> สู่รัฐธรรมนูญใหม่” </span>
+		</p>
+	</div>
+	<div class="text-center mt-[12px] md:mt-[0px] md:ml-[48px] grid gap-[12px]">
+		<div>
+			<h2 class="heading-responsive-02 text-base-100">ต้องการ 50,000 ชื่อ</h2>
+			<h2 class="heading-responsive-02 text-base-100 flex justify-center">
+				ภายใน <span
+					class="w-[36px] h-[36px] mx-[4px] rounded-full bg-primary-focus text-shadow-animation animate-pulse duration-1000 infinite"
+					>7</span
+				>
+				วัน
+			</h2>
+			<p class="text-base-100 text-[12px] text-[#aaaaaa]">เดทไลน์ 20 ส.ค. 66</p>
+		</div>
+		<div>
+			<!-- TODO: add href id -->
+			<a href="/">
+				<button class="btn btn-block font-minimal text-[26px]"
+					>ลงชื่อเลย
+					<img src="/icons/pen.svg" alt="icon-pen" />
+				</button>
+			</a>
+			<!-- TODO: add href id -->
+			<a href="/">
+				<a href="/">
+					<button
+						class="btn btn-block btn-secondary font-minimal text-[26px] mt-[4px]"
+						>อาสาตั้งโต๊ะเข้าชื่อ
+						<img src="/icons/table.svg" alt="icon-table" />
+					</button>
+				</a>
+			</a>
+		</div>
+		<a
+			class="text-decoration-line: underline font-minimal text-[26px] text-info"
+			href="/#event-reasons">ทำไมต้องลงชื่อ</a
+		>
+		<SocialGroup />
+	</div>
+</div>
+
+<style>
+	@keyframes shadow-pulse {
+		100% {
+			text-shadow: 0px 0px 6px 0px #ffffff;
+		}
+	}
+</style>
