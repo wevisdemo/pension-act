6fb3e3ccea93d0ef66fca6462592eeb7385daad0
petchill <songpon111333@gmail.com>
2023-08-12 22:23:46 +0700

    feat(#2): implement section yellow and add some images

---
+++ src/components/expanded-card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let header1: string;
+	export let header2: string;
+</script>
+
+<div class="collapse collapse-plus border-base-300 bg-base-200">
+	<input type="checkbox" class="peer" />
+	<div class="collapse-title bg-primary text-primary-content text-left">
+		<h2 class="heading-responsive-02 text-base-100">{header1}</h2>
+		<h2 class="heading-responsive-02 text-secondary-focus">{header2}</h2>
+	</div>
+	<div class="collapse-content bg-primary text-primary-content">
+		<slot />
+	</div>
+</div>
