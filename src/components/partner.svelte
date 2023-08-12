458b47075ebc46905042fe3e7fb8208bc9cb8a9c
petchill <songpon111333@gmail.com>
2023-08-13 00:49:22 +0700

    feat(#5): add partners page

---
+++ src/components/partner.svelte
@@ -0,0 +1,29 @@
+<script lang="ts"></script>
+
+<div class="grid gap-[12px] text-center">
+	<p class="font-minimal text-[20px] leading-[26px]">ผู้จัดแคมเปญ</p>
+	<h2 class="font-minimal text-[28px] leading-[26px]">
+		กลุ่มประชาชนร่างรัฐธรรมนูญ
+	</h2>
+	<p>
+		เป็นการรวมตัวเฉพาะกิจของเครือข่ายนักกิจกรรม
+		และองค์กรเอ็นจีโอหลายแห่งที่กังวลต่อการจัดตั้งรัฐบาล
+		และแนวทางการเขียนรัฐธรรมนูญใหม่ เช่น
+	</p>
+	<div class="flex flex-wrap gap-[8px] justify-center">
+		<img src="/partners/logo-01.svg" alt="partners-logo-01" />
+		<img src="/partners/logo-02.svg" alt="partners-logo-02" />
+		<img src="/partners/logo-03.svg" alt="partners-logo-03" />
+		<img src="/partners/logo-04.svg" alt="partners-logo-04" />
+		<img src="/partners/logo-05.svg" alt="partners-logo-05" />
+		<img src="/partners/logo-06.svg" alt="partners-logo-06" />
+		<img src="/partners/logo-07.svg" alt="partners-logo-07" />
+		<img src="/partners/logo-08.svg" alt="partners-logo-08" />
+		<img src="/partners/logo-09.svg" alt="partners-logo-09" />
+		<img src="/partners/logo-10.svg" alt="partners-logo-10" />
+		<img src="/partners/logo-11.svg" alt="partners-logo-11" />
+		<img src="/partners/logo-12.svg" alt="partners-logo-12" />
+		<img src="/partners/logo-13.svg" alt="partners-logo-13" />
+		<img src="/partners/logo-14.svg" alt="partners-logo-14" />
+	</div>
+</div>
--- src/components/table-register.svelte
+++ src/components/table-register.svelte
@@ -2,10 +2,10 @@
 
 <div>
 	<h1 class="heading-responsive-03 text-center">อาสาตั้งโต๊ะเข้าชื่อ</h1>
-	<div class="mt-[26px]">
+	<div class="mt-[24px]">
 		<div class="grid gap-[8px]">
 			<ol class="list-[auto]" type="1">
-				<li value="1" class="font-minimal text-[26px] leading-[26px] ml-[20px]">
+				<li value="1" class="font-minimal text-[24px] leading-[24px] ml-[20px]">
 					ลงทะเบียน
 				</li>
 			</ol>
@@ -22,7 +22,7 @@
 		<div class="w-full border-t border-base-300 my-[32px]" />
 		<div class="grid gap-[8px]">
 			<ol class="list-[auto]" type="1">
-				<li value="2" class="font-minimal text-[26px] leading-[26px] ml-[20px]">
+				<li value="2" class="font-minimal text-[24px] leading-[24px] ml-[20px]">
 					ดาวน์โหลดแบบฟอร์ม และพิมพ์ลงกระดาษ A4
 				</li>
 			</ol>
@@ -33,14 +33,14 @@
 			<img class="mx-auto" src="/register-form.svg" alt="register-form" />
 			<a target="_blank" href="https://pin.ski/3Qtz6l3">
 				<button
-					class="btn btn-block btn-primary text-[26px] leading-[26px] font-minimal"
+					class="btn btn-block btn-primary text-[24px] leading-[24px] font-minimal"
 					>ดาวน์โหลดแบบฟอร์ม
 					<img src="/icons/form.svg" alt="icon-form" />
 				</button>
 			</a>
 			<a target="_blank" href="https://shorturl.at/pszI0">
 				<button
-					class="btn btn-block btn-outline btn-primary text-[26px] leading-[26px] font-minimal"
+					class="btn btn-block btn-outline btn-primary text-[24px] leading-[24px] font-minimal"
 					>ดาวน์โหลดโปสเตอร์และโลโก้
 					<!-- svelte-ignore a11y-img-redundant-alt -->
 					<img src="/icons/image.svg" alt="icon-image" />
@@ -50,7 +50,7 @@
 		<div class="w-full border-t border-base-300 my-[32px]" />
 		<div class="grid gap-[8px]">
 			<ol class="list-[auto]" type="1">
-				<li value="3" class="font-minimal text-[26px] leading-[26px] ml-[20px]">
+				<li value="3" class="font-minimal text-[24px] leading-[24px] ml-[20px]">
 					ส่งเอกสารรวบรวมรายชื่อมายังสำนักงาน iLaw ภายในเวลา 20.00 น. ของวันที่
 					20 สิงหาคม 2566
 				</li>
@@ -58,7 +58,7 @@
 			<p>3.1. นำส่งด้วยตัวเองที่ iLaw</p>
 			<div class="flex justify-center py-[8px]">
 				<a
-					class="text-center font-minimal text-[26px] leading-[26px] underline text-[-webkit-link]"
+					class="text-center font-minimal text-[24px] leading-[24px] underline text-[-webkit-link]"
 					target="_blank"
 					href="https://goo.gl/maps/PM5nXuVFUv5GCUZw7">ดูแผนที่</a
 				>
@@ -75,14 +75,14 @@
 		<div class="w-full border-t border-base-300 my-[32px]" />
 		<div class="grid gap-[8px]">
 			<ol class="list-[auto]" type="1">
-				<li value="4" class="font-minimal text-[26px] leading-[26px] ml-[20px]">
+				<li value="4" class="font-minimal text-[24px] leading-[24px] ml-[20px]">
 					หากมีคำถามหรือสงสัย ติดต่อสอบถามผ่านไลน์
 				</li>
 			</ol>
 			<a target="_blank" href="https://lin.ee/LZObBDv">
 				<button
-					class="btn btn-block btn-secondary text-[26px] leading-[26px] font-minimal"
-					>ดาวน์โหลดแบบฟอร์ม
+					class="btn btn-block btn-secondary text-[24px] leading-[24px] font-minimal normal-case"
+					>@conforall
 					<img src="/line-color-logo.svg" alt="line-color-logo" />
 				</button>
 			</a>
