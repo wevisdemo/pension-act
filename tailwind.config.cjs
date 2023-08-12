cb6684d6621369f837fcf58b66132d49e3b16590
Th1nkK1D <witheep@gmail.com>
2023-08-12 11:42:11 +0700

    feat: config color and typography system

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,25 +1,32 @@
+const codeforall = {
+	primary: '#2E3283',
+	'primary-focus': '#4349BA',
+	secondary: '#2BB25C',
+	'secondary-focus': '#35D26E',
+	accent: '#8054FF',
+	'accent-focus': '#5929E3',
+	neutral: '#000000',
+	'base-100': '#FFFFFF',
+	'base-300': '#E5E6E6',
+	info: '#F8EB54',
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: codeforall,
+		fontFamily: {
+			sans: 'IBM Plex Sans Thai, sans-serif',
+			looped: 'IBM Plex Sans Thai Looped, sans-serif',
+			minimal: 'FC Minimal, serif',
+		},
 	},
 	plugins: [require('daisyui')],
 	daisyui: {
 		themes: [
 			{
-				codeforall: {
-					primary: '#2E3283',
-					'primary-focus': '#4349BA',
-					secondary: '#2BB25C',
-					'secondary-focus': '#35D26E',
-					accent: '#8054FF',
-					'accent-focus': '#5929E3',
-					neutral: '#000000',
-					'base-100': '#FFFFFF',
-					'base-300': '#E5E6E6',
-					info: '#F8EB54',
-				},
+				codeforall,
 			},
 		],
 	},
