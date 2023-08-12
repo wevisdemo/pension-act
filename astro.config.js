423e7a4d91c26b613427fcf6ab21e09754e5797f
Th1nkK1D <witheep@gmail.com>
2023-08-12 12:05:46 +0700

    feat: add plausible with partytown

--- astro.config.js
+++ astro.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), tailwind()],
+	integrations: [svelte(), tailwind(), partytown()],
 });
