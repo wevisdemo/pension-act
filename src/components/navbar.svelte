cf2745206b698fff2c23d0b3e63241558241dea7
petchill <songpon111333@gmail.com>
2023-08-13 12:35:30 +0700

    feat(#8): add navbar

---
+++ src/components/navbar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import SocialGroup from './social-group.svelte';
+</script>
+
+<div
+	class="w-full max-w-[864px] flex justify-between items-center h-[60px] md:h-[80px] p-[18px]"
+>
+	<div class="flex items-center">
+		<span class="hidden md:block text-base-100 heading-03 mr-[12px]"
+			>แชร์ให้เพื่อน</span
+		>
+		<SocialGroup />
+	</div>
+	<div class="max-w-[158px] md:max-w-[240px] flex-1">
+		<a href="#petition">
+			<button class="btn btn-block heading-03 font-bold"
+				>ลงชื่อเลย
+				<img src="/icons/pen.svg" alt="icon-pen" />
+			</button>
+		</a>
+	</div>
+</div>
