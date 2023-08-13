a409e247de63d0cad93c9a8c093e3e2106ee31b2
Narat S <me@mixth.net>
2023-08-13 12:37:05 +0700

    feat(#3): show submitted and error states as modals

--- src/components/signature-form.svelte
+++ src/components/signature-form.svelte
@@ -9,16 +9,17 @@
 
 	let signatureCanvas: HTMLCanvasElement;
 	let signaturePad: SignaturePad;
+	let successDialog: HTMLDialogElement;
+	let errorDialog: HTMLDialogElement;
 	let signatureEnabled = false;
-	let isSending = false;
-	let isError = false;
+	let isLoading = false;
 	const canvasResizeObserver = new ResizeObserver((entries) => {
 		signatureCanvas.width = entries[0].target.clientWidth
 		signatureCanvas.height = entries[0].target.clientHeight
 		signaturePad.fromData(signaturePad.toData());
 	});
 
-	const { form, setTouched, setData, data } = createForm({
+	const { form, setTouched, setData, data, reset } = createForm({
 		validate(values) {
 			try {
 				parse(documentSchema, values);
@@ -28,15 +29,16 @@
 			return {};
 		},
 		async onSubmit(values) {
+			isLoading = true;
 			try {
-				isError = false;
-				isSending = true;
 				await submitDocument(parse(documentSchema, values));
+				successDialog.showModal();
+				clearPad();
+				reset();
 			} catch (e) {
-				isError = true;
-				alert(e);
+				errorDialog.showModal();
 			}
-			isSending = false;
+			isLoading = false;
 		},
 		extend: reporter,
 	});
@@ -66,7 +68,7 @@
 					type="string"
 					name="personalid"
 					class="input rounded-sm bg-base-200 {messages ? 'input-error' : ''}"
-					disabled={isSending}
+					disabled={isLoading}
 				/>
 				<div class="label">
 					<span class="label-text-alt {messages ? 'text-error' : ''}"
@@ -79,7 +81,7 @@
 				<label class="label" for="prefix">
 					<span class="label-text heading-03">คำนำหน้า</span>
 				</label>
-				<select class="select rounded-sm max-w-xs bg-base-200" disabled={isSending} name="prefix">
+				<select class="select rounded-sm max-w-xs bg-base-200" disabled={isLoading} name="prefix">
 					<option selected />
 					<option>นาย</option>
 					<option>นาง</option>
@@ -95,7 +97,7 @@
 						type="text"
 						name="firstname"
 						class="input rounded-sm bg-base-200 w-full {messages ? 'input-error' : ''}"
-						disabled={isSending}
+						disabled={isLoading}
 					/>
 					<div class="label">
 						<span class="label-text-alt {messages ? 'text-error' : ''}"
@@ -113,7 +115,7 @@
 				type="text"
 				name="lastname"
 				class="input rounded-sm bg-base-200 {messages ? 'input-error' : ''}"
-				disabled={isSending}
+				disabled={isLoading}
 			/>
 			<div class="label">
 				<span class="label-text-alt {messages ? 'text-error' : ''}"
@@ -126,12 +128,12 @@
 				<span class="label-text heading-03">ลงลายมือชื่อ*</span>
 			</div>
 			<div class="relative">
-				<canvas class="bg-base-200 h-[258px] w-full rounded-sm {!signatureEnabled || isSending ? 'pointer-events-none' : ''}" bind:this={signatureCanvas} />
+				<canvas class="bg-base-200 h-[258px] w-full rounded-sm {!signatureEnabled || isLoading ? 'pointer-events-none' : ''}" bind:this={signatureCanvas} />
 				{#if signatureEnabled}
 					<button type="button" class="btn btn-outline btn-accent absolute right-[10px] bottom-4" on:click={clearPad}>
 						ล้าง
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-							<rect width="16" height="16" fill="white" style="mix-blend-mode:multiply"/>
+							<rect width="16" height="16" fill="transparent" style="mix-blend-mode:multiply"/>
 							<path d="M9 14C10.1867 14 11.3467 13.6481 12.3334 12.9888C13.3201 12.3295 14.0892 11.3925 14.5433 10.2961C14.9974 9.19975 15.1162 7.99335 14.8847 6.82946C14.6532 5.66558 14.0818 4.59648 13.2426 3.75736C12.4035 2.91825 11.3344 2.3468 10.1705 2.11529C9.00666 1.88378 7.80026 2.0026 6.7039 2.45673C5.60754 2.91085 4.67047 3.67989 4.01118 4.66658C3.35189 5.65328 3 6.81331 3 8V11.1L1.2 9.3L0.5 10L3.5 13L6.5 10L5.8 9.3L4 11.1V8C4 7.0111 4.29324 6.0444 4.84265 5.22215C5.39206 4.39991 6.17295 3.75904 7.08658 3.38061C8.00021 3.00217 9.00555 2.90315 9.97545 3.09608C10.9454 3.289 11.8363 3.76521 12.5355 4.46447C13.2348 5.16373 13.711 6.05465 13.9039 7.02455C14.0969 7.99446 13.9978 8.99979 13.6194 9.91342C13.241 10.8271 12.6001 11.6079 11.7779 12.1574C10.9556 12.7068 9.98891 13 9 13V14Z" fill="#8054FF"/>
 						</svg>
 					</button>
@@ -162,21 +164,9 @@
 				>
 			</label>
 		</div>
-		{#if isError}
-			<div class="label">
-				<span class="label-text-alt text-error">เกิดข้อผิดพลาดในการลงชื่อ โปรดลองลงชื่อใหม่อีกครั้ง</span>
-			</div>
-		{/if}
-		<button type="submit" class="btn btn-primary w-full mt-2 heading-03 text-base text-base-100 disabled:text-base-100 {isError ? 'bg-error' : 'red'}" disabled={!$data.consent || isSending}>
-			{#if !isSending && !isError}
+		<button type="submit" class="btn btn-primary w-full mt-2 heading-03 text-base text-base-100 disabled:text-base-100" disabled={!$data.consent || isLoading}>
+			{#if !isLoading}
 				ลงชื่อเลย
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-					<path fill-rule="evenodd" clip-rule="evenodd" d="M5.707 13.7071C5.59752 13.8169 5.464 13.8998 5.317 13.9491L2.317 14.9491C2.14074 15.0081 1.95153 15.0169 1.7706 14.9743C1.58966 14.9318 1.42417 14.8396 1.29268 14.7082C1.1612 14.5769 1.06894 14.4114 1.02625 14.2305C0.983555 14.0496 0.992126 13.8604 1.051 13.6841L2.051 10.6841C2.1002 10.5368 2.18305 10.4029 2.293 10.2931L10.086 2.50012C10.4611 2.12518 10.9697 1.91455 11.5 1.91455C12.0303 1.91455 12.5389 2.12518 12.914 2.50012L13.5 3.08612C13.8749 3.46118 14.0856 3.9698 14.0856 4.50012C14.0856 5.03045 13.8749 5.53907 13.5 5.91412L5.707 13.7071ZM3 11.0001L10.793 3.20712C10.9805 3.01965 11.2348 2.91434 11.5 2.91434C11.7652 2.91434 12.0195 3.01965 12.207 3.20712L12.793 3.79312C12.9805 3.98065 13.0858 4.23496 13.0858 4.50012C13.0858 4.76529 12.9805 5.0196 12.793 5.20712L5 13.0001L2 14.0001L3 11.0001Z" fill="white"/>
-					<path fill-rule="evenodd" clip-rule="evenodd" d="M9.85402 2.55999C9.80757 2.51342 9.7524 2.47648 9.69165 2.45127C9.63091 2.42607 9.56579 2.41309 9.50002 2.41309C9.43425 2.41309 9.36913 2.42607 9.30839 2.45127C9.24764 2.47648 9.19247 2.51342 9.14602 2.55999L5.85402 5.85499C5.76013 5.94887 5.6328 6.00162 5.50002 6.00162C5.36725 6.00162 5.23991 5.94887 5.14602 5.85499C5.05213 5.7611 4.99939 5.63376 4.99939 5.50099C4.99939 5.36821 5.05213 5.24087 5.14602 5.14699L8.44002 1.85399C8.57932 1.7146 8.74472 1.60402 8.92677 1.52858C9.10882 1.45314 9.30396 1.41431 9.50102 1.41431C9.69808 1.41431 9.89322 1.45314 10.0753 1.52858C10.2573 1.60402 10.4227 1.7146 10.562 1.85399L10.855 2.14599C10.9489 2.23974 11.0017 2.36695 11.0018 2.49963C11.0019 2.63231 10.9493 2.7596 10.8555 2.85349C10.7618 2.94737 10.6346 3.00017 10.5019 3.00026C10.3692 3.00036 10.2419 2.94774 10.148 2.85399L9.85502 2.56099L9.85402 2.55999Z" fill="white"/>
-					<path d="M13.293 1.20709C13.4805 1.01962 13.7348 0.914307 14 0.914307C14.2652 0.914307 14.5195 1.01962 14.707 1.20709L14.737 1.23709C14.9193 1.41933 15.0241 1.66489 15.0297 1.92257C15.0353 2.18024 14.9412 2.43013 14.767 2.62009L13.5 4.00009L12 2.50009L13.293 1.20709Z" fill="white"/>
-				</svg>
-			{:else if !isSending && isError}
-				ลองลงชื่อใหม่อีกครั้ง
 				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
 					<path fill-rule="evenodd" clip-rule="evenodd" d="M5.707 13.7071C5.59752 13.8169 5.464 13.8998 5.317 13.9491L2.317 14.9491C2.14074 15.0081 1.95153 15.0169 1.7706 14.9743C1.58966 14.9318 1.42417 14.8396 1.29268 14.7082C1.1612 14.5769 1.06894 14.4114 1.02625 14.2305C0.983555 14.0496 0.992126 13.8604 1.051 13.6841L2.051 10.6841C2.1002 10.5368 2.18305 10.4029 2.293 10.2931L10.086 2.50012C10.4611 2.12518 10.9697 1.91455 11.5 1.91455C12.0303 1.91455 12.5389 2.12518 12.914 2.50012L13.5 3.08612C13.8749 3.46118 14.0856 3.9698 14.0856 4.50012C14.0856 5.03045 13.8749 5.53907 13.5 5.91412L5.707 13.7071ZM3 11.0001L10.793 3.20712C10.9805 3.01965 11.2348 2.91434 11.5 2.91434C11.7652 2.91434 12.0195 3.01965 12.207 3.20712L12.793 3.79312C12.9805 3.98065 13.0858 4.23496 13.0858 4.50012C13.0858 4.76529 12.9805 5.0196 12.793 5.20712L5 13.0001L2 14.0001L3 11.0001Z" fill="white"/>
 					<path fill-rule="evenodd" clip-rule="evenodd" d="M9.85402 2.55999C9.80757 2.51342 9.7524 2.47648 9.69165 2.45127C9.63091 2.42607 9.56579 2.41309 9.50002 2.41309C9.43425 2.41309 9.36913 2.42607 9.30839 2.45127C9.24764 2.47648 9.19247 2.51342 9.14602 2.55999L5.85402 5.85499C5.76013 5.94887 5.6328 6.00162 5.50002 6.00162C5.36725 6.00162 5.23991 5.94887 5.14602 5.85499C5.05213 5.7611 4.99939 5.63376 4.99939 5.50099C4.99939 5.36821 5.05213 5.24087 5.14602 5.14699L8.44002 1.85399C8.57932 1.7146 8.74472 1.60402 8.92677 1.52858C9.10882 1.45314 9.30396 1.41431 9.50102 1.41431C9.69808 1.41431 9.89322 1.45314 10.0753 1.52858C10.2573 1.60402 10.4227 1.7146 10.562 1.85399L10.855 2.14599C10.9489 2.23974 11.0017 2.36695 11.0018 2.49963C11.0019 2.63231 10.9493 2.7596 10.8555 2.85349C10.7618 2.94737 10.6346 3.00017 10.5019 3.00026C10.3692 3.00036 10.2419 2.94774 10.148 2.85399L9.85502 2.56099L9.85402 2.55999Z" fill="white"/>
@@ -184,15 +174,26 @@
 				</svg>
 			{:else}
 				กำลังลงชื่อ...
-				<span class="loading loading-spinner"></span>
+				<span class="loading loading-spinner" />
 			{/if}
 		</button>
 	</div>
 
+	<dialog id="success-dialog" bind:this={successDialog} class="modal modal-bottom sm:modal-middle">
+		<form method="dialog" class="modal-box">
+			<p class="py-4">ลงชื่อสำเร็จ</p>
+			<div class="modal-action">
+				<button class="btn">ปิด</button>
+			</div>
+		</form>
+	</dialog>
 	
-
-
-
-
-
+	<dialog id="error-dialog" bind:this={errorDialog} class="modal modal-bottom sm:modal-middle">
+		<form method="dialog" class="modal-box">
+			<p class="py-4">เกิดข้อผิดพลาดในการลงชื่อ โปรดลองลงชื่อใหม่อีกครั้ง</p>
+			<div class="modal-action">
+				<button class="btn">ปิด</button>
+			</div>
+		</form>
+	</dialog>
 </form>
