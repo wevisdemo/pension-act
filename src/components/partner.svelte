35fa9157bd26588ff6e3d3a7447d74bc8290630f
Narat S <me@mixth.net>
2023-08-13 17:11:23 +0700

    feat: add more partner logos and adjust svg logos from 44x45 to 45x45

--- src/components/partner.svelte
+++ src/components/partner.svelte
@@ -25,5 +25,19 @@
 		<img src="/partners/logo-12.svg" alt="partners-logo-12" />
 		<img src="/partners/logo-13.svg" alt="partners-logo-13" />
 		<img src="/partners/logo-14.svg" alt="partners-logo-14" />
+		<img src="/partners/logo-15.png" width="45" height="45" alt="partners-logo-15" />
+		<img src="/partners/logo-16.png" width="45" height="45" alt="partners-logo-16" />
+		<img src="/partners/logo-17.png" width="45" height="45" alt="partners-logo-17" />
+		<img src="/partners/logo-18.png" width="45" height="45" alt="partners-logo-18" />
+		<img src="/partners/logo-19.png" width="45" height="45" alt="partners-logo-19" />
+		<img src="/partners/logo-20.png" width="45" height="45" alt="partners-logo-20" />
+		<img src="/partners/logo-21.png" width="45" height="45" alt="partners-logo-21" />
+		<img src="/partners/logo-22.png" width="45" height="45" alt="partners-logo-22" />
+		<img src="/partners/logo-23.png" width="45" height="45" alt="partners-logo-23" />
+		<img src="/partners/logo-24.png" width="45" height="45" alt="partners-logo-24" />
+		<img src="/partners/logo-25.png" width="45" height="45" alt="partners-logo-25" />
+		<img src="/partners/logo-26.png" width="45" height="45" alt="partners-logo-26" />
+		<img src="/partners/logo-27.png" width="45" height="45" alt="partners-logo-27" />
+		<img src="/partners/logo-28.png" width="45" height="45" alt="partners-logo-28" />
 	</div>
 </div>
