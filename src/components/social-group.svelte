85744d61088a9dce4bf2ff8ff57ecdb53cfa3d8c
Th1nkK1D <witheep@gmail.com>
2023-08-18 16:36:20 +0700

    perf: set url constant for social group

--- src/components/social-group.svelte
+++ src/components/social-group.svelte
@@ -1,31 +1,19 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
-	export let url: string = '';
-	let encodedURL = '';
-
-	onMount(() => {
-		encodedURL = encodeURIComponent(url || window.location.href);
-	});
+	const PROD_URL = encodeURI('https://conforall.com');
 </script>
 
 <div class="flex [&>*:not(:first-child)]:ml-[4px] justify-center">
 	<a
-		href={`http://www.facebook.com/sharer/sharer.php?u=${encodedURL}`}
+		href={`http://www.facebook.com/sharer/sharer.php?u=${PROD_URL}`}
 		target="_blank"
 	>
 		<img src="/facebook-logo.svg" alt="facebook-logo" />
 	</a>
-	<a
-		href={`https://twitter.com/intent/tweet?url=${encodedURL}`}
-		target="_blank"
-	>
+	<a href={`https://twitter.com/intent/tweet?url=${PROD_URL}`} target="_blank">
 		<img src="/x-logo.svg" alt="x-logo" />
 	</a>
 	<a
-		href={`https://social-plugins.line.me/lineit/share?url=${encodedURL}`}
+		href={`https://social-plugins.line.me/lineit/share?url=${PROD_URL}`}
 		target="_blank"><img src="/line-logo.svg" alt="line-logo" /></a
 	>
 </div>
-
-<style></style>
