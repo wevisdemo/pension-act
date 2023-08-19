914b428b4032b7914fbb5aab61a090e49b9d123e
petchill <songpon111333@gmail.com>
2023-08-19 14:27:24 +0700

    fix(#17): edit partner logo

--- src/components/partner.svelte
+++ src/components/partner.svelte
@@ -11,33 +11,185 @@
 		และแนวทางการเขียนรัฐธรรมนูญใหม่ เช่น
 	</p>
 	<div class="flex flex-wrap gap-[8px] justify-center">
-		<img src="/partners/logo-01.svg" alt="partners-logo-01" />
-		<img src="/partners/logo-02.svg" alt="partners-logo-02" />
-		<img src="/partners/logo-03.svg" alt="partners-logo-03" />
-		<img src="/partners/logo-04.svg" alt="partners-logo-04" />
-		<img src="/partners/logo-05.svg" alt="partners-logo-05" />
-		<img src="/partners/logo-06.svg" alt="partners-logo-06" />
-		<img src="/partners/logo-07.svg" alt="partners-logo-07" />
-		<img src="/partners/logo-08.svg" alt="partners-logo-08" />
-		<img src="/partners/logo-09.svg" alt="partners-logo-09" />
-		<img src="/partners/logo-10.svg" alt="partners-logo-10" />
-		<img src="/partners/logo-11.svg" alt="partners-logo-11" />
-		<img src="/partners/logo-12.svg" alt="partners-logo-12" />
-		<img src="/partners/logo-13.svg" alt="partners-logo-13" />
-		<img src="/partners/logo-14.svg" alt="partners-logo-14" />
-		<img src="/partners/logo-15.png" width="45" height="45" alt="partners-logo-15" />
-		<img src="/partners/logo-16.png" width="45" height="45" alt="partners-logo-16" />
-		<img src="/partners/logo-17.png" width="45" height="45" alt="partners-logo-17" />
-		<img src="/partners/logo-18.png" width="45" height="45" alt="partners-logo-18" />
-		<img src="/partners/logo-19.png" width="45" height="45" alt="partners-logo-19" />
-		<img src="/partners/logo-20.png" width="45" height="45" alt="partners-logo-20" />
-		<img src="/partners/logo-21.png" width="45" height="45" alt="partners-logo-21" />
-		<img src="/partners/logo-22.png" width="45" height="45" alt="partners-logo-22" />
-		<img src="/partners/logo-23.png" width="45" height="45" alt="partners-logo-23" />
-		<img src="/partners/logo-24.png" width="45" height="45" alt="partners-logo-24" />
-		<img src="/partners/logo-25.png" width="45" height="45" alt="partners-logo-25" />
-		<img src="/partners/logo-26.png" width="45" height="45" alt="partners-logo-26" />
-		<img src="/partners/logo-27.png" width="45" height="45" alt="partners-logo-27" />
-		<img src="/partners/logo-28.png" width="45" height="45" alt="partners-logo-28" />
+		<img
+			src="/partners/logo-01.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-01"
+		/>
+		<img
+			src="/partners/logo-02.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-02"
+		/>
+		<img
+			src="/partners/logo-03.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-03"
+		/>
+		<img
+			src="/partners/logo-04.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-04"
+		/>
+		<img
+			src="/partners/logo-05.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-05"
+		/>
+		<img
+			src="/partners/logo-06.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-06"
+		/>
+		<img
+			src="/partners/logo-07.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-07"
+		/>
+		<img
+			src="/partners/logo-09.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-09"
+		/>
+		<img
+			src="/partners/logo-10.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-10"
+		/>
+		<img
+			src="/partners/logo-11.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-11"
+		/>
+		<img
+			src="/partners/logo-12.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-12"
+		/>
+		<img
+			src="/partners/logo-13.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-13"
+		/>
+		<img
+			src="/partners/logo-14.svg"
+			width="45"
+			height="45"
+			alt="partners-logo-14"
+		/>
+		<img
+			src="/partners/logo-15.png"
+			width="45"
+			height="45"
+			alt="partners-logo-15"
+		/>
+		<img
+			src="/partners/logo-16.png"
+			width="45"
+			height="45"
+			alt="partners-logo-16"
+		/>
+		<img
+			src="/partners/logo-17.png"
+			width="45"
+			height="45"
+			alt="partners-logo-17"
+		/>
+		<img
+			src="/partners/logo-18.png"
+			width="45"
+			height="45"
+			alt="partners-logo-18"
+		/>
+		<img
+			src="/partners/logo-19.png"
+			width="45"
+			height="45"
+			alt="partners-logo-19"
+		/>
+		<img
+			src="/partners/logo-20.png"
+			width="45"
+			height="45"
+			alt="partners-logo-20"
+		/>
+		<img
+			src="/partners/logo-21.png"
+			width="45"
+			height="45"
+			alt="partners-logo-21"
+		/>
+		<img
+			src="/partners/logo-22.png"
+			width="45"
+			height="45"
+			alt="partners-logo-22"
+		/>
+		<img
+			src="/partners/logo-23.png"
+			width="45"
+			height="45"
+			alt="partners-logo-23"
+		/>
+		<img
+			src="/partners/logo-24.png"
+			width="45"
+			height="45"
+			alt="partners-logo-24"
+		/>
+		<img
+			src="/partners/logo-25.png"
+			width="45"
+			height="45"
+			alt="partners-logo-25"
+		/>
+		<img
+			src="/partners/logo-26.png"
+			width="45"
+			height="45"
+			alt="partners-logo-26"
+		/>
+		<img
+			src="/partners/logo-27.png"
+			width="45"
+			height="45"
+			alt="partners-logo-27"
+		/>
+		<img
+			src="/partners/logo-28.png"
+			width="45"
+			height="45"
+			alt="partners-logo-28"
+		/>
+		<img
+			src="/partners/logo-29.png"
+			width="45"
+			height="45"
+			alt="partners-logo-29"
+		/>
+		<img
+			src="/partners/logo-30.png"
+			width="45"
+			height="45"
+			alt="partners-logo-30"
+		/>
+		<img
+			src="/partners/logo-31.png"
+			width="45"
+			height="45"
+			alt="partners-logo-31"
+		/>
 	</div>
 </div>
